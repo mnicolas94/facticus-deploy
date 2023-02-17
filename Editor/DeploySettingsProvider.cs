677fc46321dfdea7f132e108c0ee4e69b7f04ab2
mnicolas94 <miguelcok27@gmail.com>
2023-02-17 10:30:51 -0500

    chore: -

--- Editor/DeploySettingsProvider.cs
+++ Editor/DeploySettingsProvider.cs
@@ -12,33 +12,17 @@ namespace Deploy.Editor
         [SettingsProvider]
         public static SettingsProvider GetSettingsProvider()
         {
-            bool existsSettings = DeploySettings.Instance != null;
-            SerializedObject so = existsSettings ? new SerializedObject(DeploySettings.Instance) : null;
-            var keywords = existsSettings ? SettingsProvider.GetSearchKeywordsFromSerializedObject(so) : new string[0];
+            var settings = DeploySettings.GetOrCreate();
+            SerializedObject so = new SerializedObject(settings);
+            var keywords = SettingsProvider.GetSearchKeywordsFromSerializedObject(so);
+            
             var provider = new SettingsProvider("Project/Facticus/Deploy", SettingsScope.Project)
             {
                 activateHandler = (searchContext, root) =>
                 {
-                    var settings = DeploySettings.GetOrCreate();
                     var inspector = new InspectorElement(settings);
                     root.Add(inspector);
                 },
-                // guiHandler = (searchContext) =>
-                // {
-                //     EditorGUILayout.Space(12);
-                //     
-                //     if (existsSettings)
-                //         GUIUtils.DrawSerializedObject(so);
-                //     else
-                //     {
-                //         var r = EditorGUILayout.GetControlRect();
-                //         if (GUI.Button(r, "Create settings"))
-                //         {
-                //             var settings = ScriptableObject.CreateInstance<DeploySettings>();
-                //             AssetDatabase.CreateAsset(settings, "Assets/DeploySettings.asset");
-                //         }
-                //     }
-                // },
                 keywords = keywords
             };
             
