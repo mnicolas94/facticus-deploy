9efb29ef91046eae48d63dd0ff06c1a8dd5d6a95
mnicolas94 <miguelcok27@gmail.com>
2023-07-31 15:52:41 -0400

    feat: add workflow version updater's debug code [no ci]

---
+++ Editor/Settings/WorkflowVersionUpdater.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace Deploy.Editor.Settings
+{
+    [InitializeOnLoad]
+    public class WorkflowVersionUpdater : IPackageManagerExtension
+    {
+        static WorkflowVersionUpdater()
+        {
+            PackageManagerExtensions.RegisterExtension(new WorkflowVersionUpdater());
+        }
+
+        public VisualElement CreateExtensionUI()
+        {
+            return null;
+        }
+
+        public void OnPackageSelectionChange(PackageInfo packageInfo)
+        {
+            Debug.Log(packageInfo.name);
+        }
+
+        public void OnPackageAddedOrUpdated(PackageInfo packageInfo)
+        {
+            Debug.Log("A package was installed or updated");
+            if (UpdatedSelf(packageInfo))
+            {
+                Debug.Log("It was Deploy");
+            }
+        }
+
+        public void OnPackageRemoved(PackageInfo packageInfo)
+        {
+            
+        }
+
+        private bool UpdatedSelf(PackageInfo packageInfo)
+        {
+            return packageInfo.name == "com.facticus.deploy";
+        }
+    }
+}
