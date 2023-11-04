2d71db7fe8f3daea64717c5334d50a4424c1548f
mnicolas94 <miguelcok27@gmail.com>
2023-11-04 10:04:37 -0400

    feat: add samples folder test: modify test.yml

---
+++ Samples/ScenesConfigPreBuild.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace Deploy.Samples
+{
+    public class ScenesConfigPreBuild : IPreprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            // configurar escenas
+        }
+    }
+}
---
+++ Samples/ScenesOverrideVariablesExample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Deploy.Samples
+{
+    public class ScenesOverrideVariablesExample : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [SerializeField] private List<SceneAsset> _scenes;
+#endif
+    }
+}
